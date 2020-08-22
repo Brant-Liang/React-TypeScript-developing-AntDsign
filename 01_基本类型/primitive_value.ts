c741c74e0a2d92c2280b0630f0a7cf48721fc6fd
Brant-Liang <1351805238@qq.com>
2020-08-22 23:38:41 +0800

    learn ts

---
+++ 01_基本类型/primitive_value.ts
@@ -0,0 +1,14 @@
+// 布尔类型
+let isDone: boolean = false
+
+let age: number = 12
+let binaryNumber: number = 0b1111
+
+let firstName: string = 'Brant'
+let message: string = `hello, ${firstName}, age is ${age}`
+
+let u: undefined = undefined
+let n: null = null
+
+let num: number = undefined
+let notSure: any = 4
